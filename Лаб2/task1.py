9ebd89093357f54f34f079d3bea96bff261c7f06
ShefNika <98428034+ShefNika@users.noreply.github.com>
2024-10-26 15:07:46 +0300

    Add files via upload

---
+++ Лаб2/task1.py
@@ -0,0 +1,11 @@
+money_capital = 20000  # Подушка безопасности
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+increase = 0.05  # Ежемесячный рост цен
+k = 0
+
+while spend < money_capital + salary:
+    money_capital -= (spend - salary)
+    spend *= (1 + increase)
+    k += 1
+print("Количество месяцев, которое можно протянуть без долгов:", k)
