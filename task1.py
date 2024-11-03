681331b925639617d2dba3f5b1908757fa8db41b
ShefNika <98428034+ShefNika@users.noreply.github.com>
2024-11-03 21:37:21 +0300

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,15 @@
+def find_purchase(list_, s):
+    for i, str in enumerate(list_):
+        if str == s:
+            return i
+    return None
+
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = find_purchase(items_list, find_item)
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
